9e4e4c4c7227cd9ed7fd7bae3a4222c520df65c7
djpluki <djpluki@gmail.com>
2019-12-24 10:48:15 +0100

    clean linter console

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,5 @@ import { store } from './src/store';
 
 // eslint-disable-next-line react/prop-types
 export const wrapRootElement = ({ element }) => {
-  console.log({ store });
   return <Provider store={store}>{element}</Provider>;
 };
