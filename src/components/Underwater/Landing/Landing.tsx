069e39e2e67e34c1e2238841b32572de69a6247b
djpluki <djpluki@gmail.com>
2019-12-30 15:53:23 +0100

    make tester

--- src/components/Underwater/Landing/Landing.tsx
+++ src/components/Underwater/Landing/Landing.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { times } from 'lodash';
+import { Stage, Layer, Rect } from 'react-konva';
+import Konva from 'konva';
 
 import { Title } from '../Title/Title';
 import { IntroDrop } from '../Drops/IntroDrop/IntroDrop';
 import { Starter } from '../Starter/Starter';
+// import { IntroDrop } from '../Drop/IntroDrop/IntroDrop';
+import { Tester } from './Tester';
 
 interface Props {
   isIntro: boolean;
   isStarter: boolean;
 }
 
-export const Landing = ({ isIntro, isStarter }: Props) => (
-  <>
-    {times(40, i => (
-      <IntroDrop key={`drop-${i}`} />
-    ))}
-
-    {isIntro && <Title />}
-
-    {isStarter && <Starter />}
-  </>
-);
+export const Landing = ({ isIntro, isStarter }: Props) => {
+  return (
+    <>
+      {/* {times(1, i => (
+        <IntroDrop key={`drop-${i}`} />
+      ))}
+      
+      {isIntro && <Title />}
+  
+      {isStarter && <Starter />} */}
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          {times(40, index => (
+            <Tester key={index} />
+          ))}
+        </Layer>
+      </Stage>
+    </>
+  );
+};
