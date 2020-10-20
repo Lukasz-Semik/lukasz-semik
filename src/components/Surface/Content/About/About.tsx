a419bd7cbeaa95f13912016340f134714d00d8c4
Lukasz <djpluki@gmail.com>
2020-10-20 22:24:29 +0200

    finish about page

--- src/components/Surface/Content/About/About.tsx
+++ src/components/Surface/Content/About/About.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { FormattedMessage } from 'gatsby-plugin-intl';
+import React, { useEffect, useRef } from 'react';
+import { useIntl } from 'gatsby-plugin-intl';
+import gsap from 'gsap';
 import { rem } from 'polished';
 import styled from 'styled-components';
 
@@ -37,7 +38,6 @@ const InnerWrapper = styled.div`
 
 const Text = styled.p`
   margin-bottom: ${rem(5)};
-  text-indent: 2em;
   font-size: ${rem(13)};
 
   @media ${breakpoints.smUp} {
@@ -45,21 +45,80 @@ const Text = styled.p`
   }
 `;
 
+const Letter = styled.span`
+  display: inline-block;
+  margin-right: ${rem(3)};
+`;
+
 export const About = () => {
+  const { formatMessage } = useIntl();
+  const section1Text = formatMessage({ id: 'surface.sections.about.section1' });
+  const section2Text = formatMessage({ id: 'surface.sections.about.section2' });
+
+  const ref = useRef(null);
+  const itemsSection1Ref = useRef<HTMLSpanElement[] | null[]>([]);
+  const itemsSection2Ref = useRef<HTMLSpanElement[] | null[]>([]);
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    const commonProps = {
+      duration: 0.8,
+      opacity: 0,
+      scale: 0,
+      transformOrigin: '0% 50% -50',
+      ease: 'back',
+      stagger: 0.01,
+    };
+
+    tl.from(
+      itemsSection1Ref.current,
+      {
+        delay: 1,
+        y: 80,
+        rotationX: 180,
+        ...commonProps,
+      },
+      '+=0'
+    ).from(
+      itemsSection2Ref.current,
+      {
+        delay: -1,
+        rotationX: -180,
+        y: 0,
+        ...commonProps,
+      },
+      '+=0'
+    );
+  }, []);
+
   return (
     <Wrapper>
       <InnerWrapper>
         <AnimatedLine lineColor={styles.colors.black} />
-        <MountingOpacityWrapper duration={1.5}>
-          <Text>
-            <FormattedMessage id="surface.sections.about.section1" />
+        <MountingOpacityWrapper duration={0.5}>
+          <Text ref={ref}>
+            {section1Text.split(' ').map((letter, index) => (
+              <Letter
+                key={`${letter}-1`}
+                ref={element => (itemsSection1Ref.current[index] = element)}
+              >
+                {letter}
+              </Letter>
+            ))}
           </Text>
-        </MountingOpacityWrapper>
 
-        <MountingOpacityWrapper duration={1.5} delay={0.5}>
           <Text>
-            <FormattedMessage id="surface.sections.about.section2" />
+            {section2Text.split(' ').map((letter, index) => (
+              <Letter
+                key={`${letter}-2`}
+                ref={element => (itemsSection2Ref.current[index] = element)}
+              >
+                {letter}
+              </Letter>
+            ))}
           </Text>
+
           <AnimatedLine lineColor={styles.colors.black} />
         </MountingOpacityWrapper>
       </InnerWrapper>
